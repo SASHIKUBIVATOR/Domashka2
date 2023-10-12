86d2b3910f05262be1b9b0acc55e61c2218b139d
sashik <mrnormalnybog@gmail.com>
2023-10-12 20:21:13 +1000

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello, World!";
+    setlocale(LC_ALL, "RU");
+    std::cout << "Hello, World" << std::endl <<  "Привет\n";
+    std::cout << "мяу мяу тест";
     return 0;
 }
